549e17b25590a6323429d0c33b7cfc3e948f15db
Marcelo Arteaga <arteagamarcelo@gmail.com>
2021-10-02 21:57:57 -0400

    changed app name heroku collectstatic

--- base/settings.py
+++ base/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "change-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = strtobool(os.getenv("DEBUG", False))
+DEBUG = strtobool(os.getenv("DEBUG", "false"))
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "")]
 
 MIDDLEWARE = [
