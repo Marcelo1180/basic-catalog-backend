03df605b2f6951950019c594f778120f3e42ca07
Marcelo Arteaga <arteagamarcelo@gmail.com>
2021-10-03 12:35:28 -0400

    change env method

--- base/settings.py
+++ base/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "change-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY", "change-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = strtobool(os.getenv("DEBUG", "false"))
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "")]
+DEBUG = strtobool(os.environ.get("DEBUG", "true"))
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "")]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
